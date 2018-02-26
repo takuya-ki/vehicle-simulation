74710b6c6274f1645e1396452f7d9d5ffdacf2c3
Takuya Kiyokawa <taku8926@gmail.com>
2018-02-27 03:11:35 +0900

    Updated all comments in codes from Japanese to English

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # car-simulation
 
-This is a car simulation program by vehicle model of front wheel steering.
+This is a vehicle simulation program using vehicle model of front wheel steering.
 
 ## Description
 
 Depending on input target velocity and steering angle, the running path is displayed as the output result below.
 
-![result](https://github.com/takuya-ki/car-simulation/blob/media/result.gif)
+![result](https://github.com/takuya-ki/vehicle-simulation/blob/media/result.gif)
 
 ## Features
 
 
 ## Installation/Requirement
 
-	$ git clone git@github.com:takuya-ki/car-simulation.git
-	$ cd car-simulation
+	$ git clone git@github.com:takuya-ki/vehicle-simulation.git
+	$ cd vehicle-simulation
 
 - g++ 4.9.4 (g++ -dumpversion)
 - C++11
--- drive_sim.cpp
+++ drive_sim.cpp
@@ -1,7 +1,7 @@
 
 #include "drive_sim.hpp"
 
-// 入力信号の生成
+// Generate an input signal
 void creatTargetSignal() {
 
   if(u_type=="sin"){
@@ -16,11 +16,11 @@ void creatTargetSignal() {
   v_In = v_m_s;
 }
 
-// 前輪操舵車両（2ws）のダイナミクスの計算
+// Calculate the dynamics of a front-wheel steering vehicle (2ws)
 void dynamics2ws(const int t, const double St_, const double v_) {
 
-    // ********** 直接ヨーモーメント制御モデル ********** //
-    // 状態方程式の行列計算
+    // ********** Direct yaw moment control mdoel ********** //
+    // Calculate matrix of state equation
     a11 = double(-2.0)*(Cf+Cr)/m/v_;
     a12 = double(-1.0)-double(2.0)*(la*Cf-lb*Cr)/m/v_/v_;
     a21 = double(-2.0)*(la*Cf-lb*Cr)/I;
@@ -29,7 +29,7 @@ void dynamics2ws(const int t, const double St_, const double v_) {
     b11 = double(2.0)*Cf/m/v_;
     b21 = double(2.0)*la*Cf/I;
 
-    // 横滑り角度計算
+    // Calculate sideslip angle
     if(v_<=0.0) {
       //double(0.0);
     } else {
@@ -38,13 +38,13 @@ void dynamics2ws(const int t, const double St_, const double v_) {
       } else {
         beta_v[t] = (St_*n*b11 + beta_[t-1]*a11 + gamma_[t-1]*a12);
 
-        // 台形積分による積分値算出
+        // The integral value calculated by trapezoidal integration
         beta_vInteg += ((beta_v[t-1]+beta_v[t])*dt/double(2.0));
         beta_[t] = beta_vInteg;
       }
     }
 
-    // ヨーレート計算
+    // Calculate yaw rate
     if(v_<=0.0) {
       //double(0.0);
     } else {
@@ -53,13 +53,14 @@ void dynamics2ws(const int t, const double St_, const double v_) {
       } else {
         gamma_a[t] = (St_*n*b21 + beta_[t-1]*a21 + gamma_[t-1]*a22);
 
-        // 台形積分による積分値算出
+        // The integral value calculated by trapezoidal integration
         gamma_aInteg += ((gamma_a[t-1]+gamma_a[t])*dt/double(2.0));
         gamma_[t] = gamma_aInteg;
       }
     }
 
-    // *** 車両の横方向の運動方程式(横加速度と横滑り角の関係) *** //
+    // *** lateral motion of equation for a vehicle *** //
+    // (Relationship between lateral acceleration and sideslip angle)
     c11 = double(0.0);
     c12 = double(1.0);
     c21 = v_*a11;
@@ -68,11 +69,12 @@ void dynamics2ws(const int t, const double St_, const double v_) {
     d11 = double(0.0);
     d21 = v_*b11;
 
-    // 横加速度の計算
+    // lateral acceleration
     ay[t] = (St_*n*d21 + beta_[t-1]*c21 + gamma_[t-1]*c22);
 
-    // ***** 車両全体の運動方程式から各変位の計算(台形積分利用) ***** //
-    yaw_ += ((gamma_[t-1]+gamma_[t])*dt/double(2.0)); // ヨー角
+    // ***** Calculate each displacement from the equation of motion of the whole vehicle ***** //
+    // (Use trapezoidal integration)
+    yaw_ += ((gamma_[t-1]+gamma_[t])*dt/double(2.0)); // yaw angle
     theta[t] = yaw_;
 
     vx_[t] = (cos( beta_[t] + theta[t] ) * v_);
@@ -84,27 +86,28 @@ void dynamics2ws(const int t, const double St_, const double v_) {
     py[t] = vy_Integ;
 }
 
-// MV2単体での車両モデルとダイナミクスに基づく走行シミュレーション
+// Running simulation based on vehicle model and dynamics with MV2 alone
 void drivingMV2_1(){
 
-  // 目標信号の生成
+  // Generate a target signal
   creatTargetSignal();
 
-  // Animation Settings(by gnuplot)
-  FILE *gp = popen("gnuplot", "w");  // popen(): 標準入力に書き込む
+  // Animation Settings (by gnuplot)
+  FILE *gp = popen("gnuplot", "w");
   if (gp == NULL) { exit(1); }
-  // 現在の対話型出力形式に対してマウス機能を有効にする．
-  // 対話型モードでは通常デフォルトでこれは有効だが,コマンドがファイルから読み込まれる場合は無効
+  // Enable the mouse function for the current interactive output format
+  // In interactive mode this is usually enabled by default
+  // but invalid if the command is read from a file
   fputs("set mouse\n", gp);
-  fputs("set ticslevel 0\n", gp); // z軸の0点がXY平面から離れるのを防ぐ
-  fputs("set title 'running path'\n", gp);  // タイトルを設定
+  fputs("set ticslevel 0\n", gp); // Prevent the 0 point of the z axis from leaving the XY plane
+  fputs("set title 'running path'\n", gp);  // Set title
   fputs("set xlabel 'x[m]'\n", gp);
   fputs("set xrange [0:30]\n", gp);
   fputs("set ylabel 'y[m]'\n", gp);
   fputs("set yrange [0:2.5]\n", gp);
   char fname[256] = "running_path.dat";
   std::ofstream fs;
-  
+
 
   std::cout << "time vector: " << time_vec << std::endl;
   std::cout << "steering angle[rad]: " << StAngle_r << std::endl;
@@ -113,26 +116,26 @@ void drivingMV2_1(){
   for(int sim_time=1; sim_time<time_vec; sim_time++) {
     dynamics2ws(sim_time, St_In[sim_time], v_In);
 
-    // 目標信号の結果出力
+    // Output the result of target signal
     //fs << std::to_string(sim_time*dt) << " "
-    //   << std::to_string(St_In[sim_time]) << std::endl; // 位置座標をファイルに書き込み
+    //   << std::to_string(St_In[sim_time]) << std::endl; // Write position coordinates to file
 
-    // ダイナミクスシミュレーション結果出力（gnuplot）
+    // Output the result of dynamics simulation（gnuplot）
     fputs("plot \"-\" w p pt 7 ps 3\n", gp);
     fprintf(gp, "%lf %lf\n", px[sim_time],py[sim_time]);
-    fputs("e\n", gp); // gnuplotの終了
+    fputs("e\n", gp);
 
     fs << std::to_string(px[sim_time]) << " "
-      << std::to_string(py[sim_time]) << std::endl; // 位置座標をファイルに書き込み
-    fflush(gp);	// バッファに格納されているデータを吐き出す（必須）
-    // usleep(1000000);    // 単位は[μs]
+      << std::to_string(py[sim_time]) << std::endl; // Write position coordinates to file
+    fflush(gp);	// Spit out data stored in buffer (mandatory)
+    // usleep(1000000);    // Unit is [μs]
   }
 
-  fs.close();	// 位置を書いているファイルを閉じる
+  fs.close();	// Close the file describing the position
   fputs("plot 'running_path.dat' w p pt 7 ps 1.5 lc rgb \"blue\"\n", gp);
-  fflush(gp); // バッファに格納されているデータを吐き出す（必須）
+  fflush(gp); // Spit out data stored in buffer (mandatory)
   std::cin.get();
-  fputs("exit\n", gp); // gnuplotの終了
-  fflush(gp); // バッファに格納されているデータを吐き出す（必須）
+  fputs("exit\n", gp);
+  fflush(gp); // Spit out data stored in buffer (mandatory)
   pclose(gp);
 }
